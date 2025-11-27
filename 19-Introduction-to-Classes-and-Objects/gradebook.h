e4e51320b492d5e7b1db00a891167a49644658d4
Alper <alper.makcan@gmail.com>
2025-11-27 17:43:12 +0300

    makes gradebook file a separate module

--- 19-Introduction-to-Classes-and-Objects/gradebook.h
+++ 19-Introduction-to-Classes-and-Objects/gradebook.h
@@ -32,13 +32,3 @@ public:
 private:
     string courseName;
 };
-
-int main()
-{
-    GradeBook gradeBook1("CS101 Introduction to C++ Programming");
-    GradeBook gradeBook2("CS102 Data Structures in C++");
-
-    cout << "gradeBook1 created for course: " << gradeBook1.getCourseName() << "\ngradeBook2 created for course: " << gradeBook2.getCourseName() << endl;
-
-    return 0;
-}
