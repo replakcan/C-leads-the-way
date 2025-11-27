59a73b6cdbfb0d87d6e99425d327ddfb1b075998
Alper <alper.makcan@gmail.com>
2025-11-27 23:41:39 +0300

    expand Time class imp and interface

--- 20-Classes-Deeper-Look-Part-I/Time.cpp
+++ 20-Classes-Deeper-Look-Part-I/Time.cpp
@@ -7,16 +7,46 @@ using std::setw;
 
 #include "Time.h"
 
-Time::Time()
+Time::Time(int hr, int min, int sec)
 {
-    hour = minute = second = 0;
+    setTime(hr, min, sec);
 }
 
 void Time::setTime(int h, int m, int s)
 {
+    setHour(h);
+    setMinute(m);
+    setSecond(s);
+}
+
+void Time::setHour(int h)
+{
     hour = (h >= 0 && h < 24) ? h : 0;
+}
+
+void Time::setMinute(int m)
+{
     minute = (m >= 0 && m < 60) ? m : 0;
+}
+
+void Time::setSecond(int s)
+{
     second = (s >= 0 && s < 60) ? s : 0;
+}
+
+int Time::getHour()
+{
+    return hour;
+}
+
+int Time::getMinute()
+{
+    return minute;
+}
+
+int Time::getSecond()
+{
+    return second;
 }
 
 void Time::printUniversal()
--- 20-Classes-Deeper-Look-Part-I/Time.h
+++ 20-Classes-Deeper-Look-Part-I/Time.h
@@ -4,8 +4,17 @@
 class Time
 {
 public:
-    Time();
+    Time(int = 0, int = 0, int = 0);
+
     void setTime(int, int, int);
+    void setHour(int);
+    void setMinute(int);
+    void setSecond(int);
+
+    int getHour();
+    int getMinute();
+    int getSecond();
+
     void printUniversal();
     void printStandard();
 
