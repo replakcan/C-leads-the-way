db93d30390f54e14561bc9945d8cfa26277f764e
Alper <alper.makcan@gmail.com>
2025-11-27 23:41:12 +0300

    private fxn member demo

---
+++ 20-Classes-Deeper-Look-Part-I/SalesPerson.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+
+#include <iomanip>
+using std::setprecision;
+
+#include "SalesPerson.h"
+
+SalesPerson::SalesPerson()
+{
+    for (int i = 0; i < 12; i++)
+    {
+        sales[i] = 0.0;
+    }
+}
+
+void SalesPerson::getSalesFromUser()
+{
+    double salesFigure;
+
+    for (int i = 1; i <= 12; i++)
+    {
+        cout << "Enter sales amount for month " << i << ": ";
+        cin >> salesFigure;
+        setSales(i, salesFigure);
+    }
+}
+
+void SalesPerson::setSales(int month, double amount)
+{
+    if (month >= 1 && month <= 12 && amount > 0)
+    {
+        sales[month - 1] = amount;
+    }
+    else
+    {
+        cout << "Invalid month or sales figure" << endl;
+    }
+}
+
+void SalesPerson::printAnnualSales()
+{
+    cout << setprecision(2) << fixed
+         << "\nThe total annual sales are: $"
+         << totalAnnualSales() << endl;
+}
+
+double SalesPerson::totalAnnualSales()
+{
+    double total = 0.0;
+
+    for (int i = 0; i < 12; i++)
+    {
+        total += sales[i];
+    }
+
+    return total;
+}
