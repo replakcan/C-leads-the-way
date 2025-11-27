520eeedd15802ada77cfdeb60505bdc76ddfcfb3
Alper <alper.makcan@gmail.com>
2025-11-27 15:56:33 +0300

    updates gradebook member function as it gets a parameter

--- 19-Introduction-to-Classes-and-Objects/gradebook.cpp
+++ 19-Introduction-to-Classes-and-Objects/gradebook.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+using std::cin;
 using std::cout;
 using std::endl;
+
+#include <string>
+using std::getline;
+using std::string;
 
 class GradeBook
 {
 public:
-    void displayMessage()
+    void displayMessage(string courseName)
     {
-        cout << "Welcome to the Grade Book!" << endl;
+        cout << "Welcome to the Grade book for " << courseName << "!" << endl;
     }
 };
 
 int main()
 {
+    string nameOfCourse;
     GradeBook myGradeBook;
-    myGradeBook.displayMessage();
+
+    cout << "Please enter the course name:" << endl;
+    getline(cin, nameOfCourse);
+
+    myGradeBook.displayMessage(nameOfCourse);
     return 0;
 }
