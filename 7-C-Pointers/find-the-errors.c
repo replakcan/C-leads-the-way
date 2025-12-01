0129ab8e749d05c348ed6c68f7c397f37b6e20c3
Alper <alper.makcan@gmail.com>
2025-12-01 23:15:48 +0300

    chapter-7 find-the-errors exercises

---
+++ 7-C-Pointers/find-the-errors.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // float *realPtr;
+    // long *integerPtr;
+
+    // integerPtr = realPtr;
+
+    // int * x, y;
+    // x = y;
+
+    /*  char s[] = "this is a char array";
+     char *sPtr = &s[0];
+     int count;
+
+     for (; *sPtr != '\0'; sPtr++)
+     {
+         printf("%c", *sPtr);
+     } */
+
+    /* short *numPtr, result;
+    void *genericPtr = numPtr;
+
+    result = *genericPtr + 7; */
+
+    /* char *s;
+    printf("%s\n", s); */
+
+    return 0;
+}
