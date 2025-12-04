148de209076ae6a0a05e41a1bc6c57f5e5e2f40a
Alper <alper.makcan@gmail.com>
2025-12-04 17:09:50 +0300

    Tests IntegerSet class on main

--- 21-Classes-Deeper-Look-Part-II/main.cpp
+++ 21-Classes-Deeper-Look-Part-II/main.cpp
@@ -3,24 +3,32 @@ using std::cout;
 using std::endl;
 
 #include "SavingsAccountInterface.h"
+#include "IntegerSet.h"
 
 int main()
 {
-    SavingsAccountInterface *savingAcc1 = new SavingsAccountInterface(2000.00);
+    // SavingsAccountInterface *savingAcc1 = new SavingsAccountInterface(2000.00);
 
-    SavingsAccountInterface *savingAcc2 = new SavingsAccountInterface(3000.00);
+    // SavingsAccountInterface *savingAcc2 = new SavingsAccountInterface(3000.00);
 
-    cout << savingAcc1->calculateMonthlyInterest() << endl;
-    cout << savingAcc2->calculateMonthlyInterest() << endl;
+    // cout << savingAcc1->calculateMonthlyInterest() << endl;
+    // cout << savingAcc2->calculateMonthlyInterest() << endl;
 
-    savingAcc1->modifyInterestRate(3);
+    // savingAcc1->modifyInterestRate(3);
 
-    cout << savingAcc1->calculateMonthlyInterest() << endl;
-    cout << savingAcc2->calculateMonthlyInterest() << endl;
+    // cout << savingAcc1->calculateMonthlyInterest() << endl;
+    // cout << savingAcc2->calculateMonthlyInterest() << endl;
 
-    double balance = savingAcc1->getSavingsBalance();
+    // double balance = savingAcc1->getSavingsBalance();
 
-    cout << balance << endl;
+    // cout << balance << endl;
+
+    IntegerSet set1 = IntegerSet();
+
+    set1.insertElement(25);
+    set1.insertElement(32);
+    set1.insertElement(69);
+    set1.printSet();
 
     return 0;
 }
